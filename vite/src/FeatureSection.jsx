464b79bd961fd71a6a83a30bfe476dcab378d669
NaviteLogger <kacprzakmarek92@gmail.com>
2024-02-08 14:40:18 +0100

    Added FeatureSection Component and its corresponding css file

---
+++ vite/src/FeatureSection.jsx
@@ -0,0 +1,15 @@
+// FeaturesSection.js
+import React from 'react';
+import FeatureCard from './FeatureCard'; // Assuming FeatureCard is already a component
+import projectStyles from './style.module.css';
+import styles from './home.module.css';
+
+const FeaturesSection = () => {
+    return (
+        <div className={projectStyles['features-container']}>
+            {/* Features section content including FeatureCard components */}
+        </div>
+    );
+};
+
+export default FeaturesSection;
