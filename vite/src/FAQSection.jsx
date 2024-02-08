a035e49b9446393475d8eb413a1d62ebc97be3ae
NaviteLogger <kacprzakmarek92@gmail.com>
2024-02-08 14:42:43 +0100

    Added FAQSection Component and its corresponding css file

---
+++ vite/src/FAQSection.jsx
@@ -0,0 +1,15 @@
+// FAQSection.js
+import React from 'react';
+import Question1 from './Question1'; // Assuming Question1 is already a component
+import projectStyles from './style.module.css';
+import styles from './home.module.css';
+
+const FAQSection = () => {
+    return (
+        <div className={projectStyles['faq-container']}>
+            {/* FAQ section content including Question1 components */}
+        </div>
+    );
+};
+
+export default FAQSection;
