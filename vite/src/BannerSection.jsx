f9f77f09a8dba5cdcc87c55924b6ac9ce0289c3a
NaviteLogger <kacprzakmarek92@gmail.com>
2024-02-08 14:41:06 +0100

    Added BannerSection Component and its corresponding css file

---
+++ vite/src/BannerSection.jsx
@@ -0,0 +1,14 @@
+// BannerSection.js
+import React from 'react';
+import projectStyles from './style.module.css';
+import styles from './home.module.css';
+
+const BannerSection = () => {
+    return (
+        <div className={`${projectStyles['banner-container']} ${styles['banner1']}`}>
+            {/* Banner section content */}
+        </div>
+    );
+};
+
+export default BannerSection;
