492aeb3c1c88deb373b6a82a8d1cbde1715b35cc
NaviteLogger <kacprzakmarek92@gmail.com>
2024-02-08 14:38:50 +0100

    Added HeroSection Component and its corresponding css file

---
+++ vite/src/HeroSection.jsx
@@ -0,0 +1,14 @@
+// HeroSection.js
+import React from 'react';
+import projectStyles from './style.module.css';
+import styles from './home.module.css';
+
+const HeroSection = () => {
+    return (
+        <div className={`${projectStyles['hero-container']} ${styles['hero1']}`}>
+            {/* Hero section content */}
+        </div>
+    );
+};
+
+export default HeroSection;
