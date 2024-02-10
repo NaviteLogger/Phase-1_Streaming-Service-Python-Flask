f6a5f6fb226583659030e40b59e107813bb8c47c
NaviteLogger <kacprzakmarek92@gmail.com>
2024-02-10 18:31:27 +0100

    Removed the Error: '...' is not defined error

--- my-app/pages/UserPanel/UserPanel.js
+++ my-app/pages/UserPanel/UserPanel.js
@@ -1,21 +1,19 @@
 // UserPanel.js
 import React from 'react';
-import MovieBrowser from './MovieBrowser'; // Component for browsing movies
-import MovieRequest from './MovieRequest'; // Component for requesting movies
-import BookmarkedMovies from './BookmarkedMovies'; // Component for displaying bookmarked movies
-import Header from './Header'; // Assuming you export your Header in index.js
-import Footer from './Footer'; // Assuming you export your Footer in index.js
+import MovieBrowser from '../MovieBrowser/MovieBrowser.js'; // Component for browsing movies
+import MovieRequest from '../MovieRequest/MovieRequest.js'; // Component for requesting movies
+import BookmarkedMovies from '../BookmarkedMovies/BookmarkedMovies.js'; // Component for displaying bookmarked movies
+//import Header from './Header'; // Assuming you export your Header in index.js
+//import Footer from './Footer'; // Assuming you export your Footer in index.js
 
 function UserPanel() {
     return (
         <div className="user-panel">
-            <Header />
             <main>
                 <MovieBrowser />
                 <MovieRequest />
                 <BookmarkedMovies />
             </main>
-            <Footer />
         </div>
     );
 }
