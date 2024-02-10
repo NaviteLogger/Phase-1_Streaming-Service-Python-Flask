d234f53c11779b24ed0de3f3211d21793e9fed28
NaviteLogger <kacprzakmarek92@gmail.com>
2024-02-10 22:34:08 +0100

    Moved the 'home-features' div into a separate component. Fixed returning statements in other files

---
+++ my-app/pages/HomeFeatures.js
@@ -0,0 +1,107 @@
+import React from "react";
+import "./styles.css";
+
+export default function HomeFeatures() {
+    return (
+        <div className="home-features">
+            <div className="featuresContainer">
+                <div className="home-features1">
+                    <div className="home-container2">
+                        <span className="overline">
+                            <span>features</span>
+                            <br></br>
+                        </span>
+                        <h2 className="home-features-heading heading2">
+                            Unleash the Power of Streaming
+                        </h2>
+                        <span className="home-features-sub-heading bodyLarge">
+                            <span>
+                                <span>
+                                    <span>
+                                        Experience the future of entertainment with our
+                                        cutting-edge features
+                                    </span>
+                                    <span>
+                                        <span
+                                            dangerouslySetInnerHTML={{
+                                                __html: ' ',
+                                            }}
+                                        />
+                                    </span>
+                                </span>
+                                <span>
+                                    <span>
+                                        <span
+                                            dangerouslySetInnerHTML={{
+                                                __html: ' ',
+                                            }}
+                                        />
+                                    </span>
+                                    <span>
+                                        <span
+                                            dangerouslySetInnerHTML={{
+                                                __html: ' ',
+                                            }}
+                                        />
+                                    </span>
+                                </span>
+                            </span>
+                            <span>
+                                <span>
+                                    <span>
+                                        <span
+                                            dangerouslySetInnerHTML={{
+                                                __html: ' ',
+                                            }}
+                                        />
+                                    </span>
+                                    <span>
+                                        <span
+                                            dangerouslySetInnerHTML={{
+                                                __html: ' ',
+                                            }}
+                                        />
+                                    </span>
+                                </span>
+                                <span>
+                                    <span>
+                                        <span
+                                            dangerouslySetInnerHTML={{
+                                                __html: ' ',
+                                            }}
+                                        />
+                                    </span>
+                                    <span>
+                                        <span
+                                            dangerouslySetInnerHTML={{
+                                                __html: ' ',
+                                            }}
+                                        />
+                                    </span>
+                                </span>
+                            </span>
+                        </span>
+                    </div>
+                    <div className="home-container3">
+                        <FeatureCard
+                            heading="Easy-to-use Interface"
+                            subHeading="Intuitive and user-friendly interface for seamless navigation"
+                        ></FeatureCard>
+                        <FeatureCard
+                            heading="High-Quality Streaming"
+                            subHeading="Enjoy your favorite content in stunning HD quality"
+                        ></FeatureCard>
+                        <FeatureCard
+                            heading="Personalized Recommendations"
+                            subHeading="Discover new shows and movies tailored to your interests"
+                        ></FeatureCard>
+                        <FeatureCard
+                            heading="Multi-Device Support"
+                            subHeading="Stream on any device, anytime, anywhere"
+                        ></FeatureCard>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -4,6 +4,7 @@ import Head from 'next/head'
 
 import HomeHeader from './HomeHeader.js';
 import HomeHero from './HomeHero.js';
+import HomeFeatures from './HomeFeatures.js';
 
 import FeatureCard from '../components/feature-card'
 import Question1 from '../components/question1'
@@ -17,106 +18,7 @@ const Home = (props) => {
         </Head>
         <HomeHeader />
         <HomeHero />
-        <div className="home-features">
-          <div className="featuresContainer">
-            <div className="home-features1">
-              <div className="home-container2">
-                <span className="overline">
-                  <span>features</span>
-                  <br></br>
-                </span>
-                <h2 className="home-features-heading heading2">
-                  Unleash the Power of Streaming
-                </h2>
-                <span className="home-features-sub-heading bodyLarge">
-                  <span>
-                    <span>
-                      <span>
-                        Experience the future of entertainment with our
-                        cutting-edge features
-                      </span>
-                      <span>
-                        <span
-                          dangerouslySetInnerHTML={{
-                            __html: ' ',
-                          }}
-                        />
-                      </span>
-                    </span>
-                    <span>
-                      <span>
-                        <span
-                          dangerouslySetInnerHTML={{
-                            __html: ' ',
-                          }}
-                        />
-                      </span>
-                      <span>
-                        <span
-                          dangerouslySetInnerHTML={{
-                            __html: ' ',
-                          }}
-                        />
-                      </span>
-                    </span>
-                  </span>
-                  <span>
-                    <span>
-                      <span>
-                        <span
-                          dangerouslySetInnerHTML={{
-                            __html: ' ',
-                          }}
-                        />
-                      </span>
-                      <span>
-                        <span
-                          dangerouslySetInnerHTML={{
-                            __html: ' ',
-                          }}
-                        />
-                      </span>
-                    </span>
-                    <span>
-                      <span>
-                        <span
-                          dangerouslySetInnerHTML={{
-                            __html: ' ',
-                          }}
-                        />
-                      </span>
-                      <span>
-                        <span
-                          dangerouslySetInnerHTML={{
-                            __html: ' ',
-                          }}
-                        />
-                      </span>
-                    </span>
-                  </span>
-                </span>
-              </div>
-              <div className="home-container3">
-                <FeatureCard
-                  heading="Easy-to-use Interface"
-                  subHeading="Intuitive and user-friendly interface for seamless navigation"
-                ></FeatureCard>
-                <FeatureCard
-                  heading="High-Quality Streaming"
-                  subHeading="Enjoy your favorite content in stunning HD quality"
-                ></FeatureCard>
-                <FeatureCard
-                  heading="Personalized Recommendations"
-                  subHeading="Discover new shows and movies tailored to your interests"
-                ></FeatureCard>
-                <FeatureCard
-                  heading="Multi-Device Support"
-                  subHeading="Stream on any device, anytime, anywhere"
-                ></FeatureCard>
-              </div>
-            </div>
-          </div>
-        </div>
+        <HomeFeatures />
         <div className="home-banner">
           <div className="bannerContainer home-banner1">
             <h1 className="home-banner-heading heading2">
