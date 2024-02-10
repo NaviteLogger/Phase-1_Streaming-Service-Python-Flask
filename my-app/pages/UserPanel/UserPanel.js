3012d415006bbb85e617c1f245dbbe39af498e3f
NaviteLogger <kacprzakmarek92@gmail.com>
2024-02-10 22:51:34 +0100

    Added Header and Footer components to UserPanel page

--- my-app/pages/UserPanel/UserPanel.js
+++ my-app/pages/UserPanel/UserPanel.js
@@ -4,19 +4,21 @@ import MovieBrowser from '../MovieBrowser/MovieBrowser.js'; // Component for bro
 import MovieRequest from '../MovieRequest/MovieRequest.js'; // Component for requesting movies
 import BookmarkedMovies from '../BookmarkedMovies/BookmarkedMovies.js'; // Component for displaying bookmarked movies
 import './UserPanel.css';
-//import Header from './Header'; // Assuming you export your Header in index.js
-//import Footer from './Footer'; // Assuming you export your Footer in index.js
+import Header from '../HomeHeader';
+import Footer from '../Footer';
 
 function UserPanel() {
     return (
-        <div className="user-panel">
-            {/* <Header /> */}
-            <main>
-                <MovieBrowser />
-                <MovieRequest />
-                <BookmarkedMovies />
-            </main>
-            {/* <Footer /> */}
+        <div className="user-panel-box">
+            <Header />
+            <div className="user-panel">
+                <main>
+                    <MovieBrowser />
+                    <MovieRequest />
+                    <BookmarkedMovies />
+                </main>
+            </div>
+            <Footer />
         </div>
     );
 }
