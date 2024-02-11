720463807af11fb991060b1cf16b1f7f47e225d4
NaviteLogger <kacprzakmarek92@gmail.com>
2024-02-11 15:09:38 +0100

    Added the 'async' keyword at the beginning of 'handleResponse' function

--- my-app/pages/LoginPage/LoginPage.js
+++ my-app/pages/LoginPage/LoginPage.js
@@ -14,7 +14,7 @@ function LoginPage() {
         setCredentials({ ...credentials, [name]: value });
     };
 
-    const handleSubmit = (event) => {
+    const handleSubmit = async (event) => {
         event.preventDefault();
 
         try {
