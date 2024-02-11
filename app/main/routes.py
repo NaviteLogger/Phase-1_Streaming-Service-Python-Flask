d949893389c0250b1805691b37f620974e7c4691
NaviteLogger <kacprzakmarek92@gmail.com>
2024-02-11 15:12:04 +0100

    Removed test_database_connection route

--- app/main/routes.py
+++ app/main/routes.py
@@ -8,9 +8,3 @@ main_bp = Blueprint("main_bp", __name__)
 @main_bp.route("/")
 def index():
     return render_template("index.html")
-
-
-@main_bp.route("/test-database-connection")
-def test_database_connection():
-    cursor = current_app.mysql.connection.cursor()
-    cursor.execute("SELECT *")
