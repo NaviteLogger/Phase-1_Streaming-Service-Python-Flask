94d75acef36012b14917898827f326bad5de04d6
NaviteLogger <kacprzakmarek92@gmail.com>
2024-02-12 15:18:31 +0100

    Added login route to users blueprint

--- app/users/routes.py
+++ app/users/routes.py
@@ -1,3 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
 from app import db
 from . import users_bp
+
+
+@users_bp.route("/login", methods=["GET"])
+def login():
+    # Parse username and password from request
+    data = request.get_json()
+    username = data.get("username")
+    password = data.get("password")
