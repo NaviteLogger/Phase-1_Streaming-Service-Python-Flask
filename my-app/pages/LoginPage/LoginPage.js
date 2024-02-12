95d89a9c5441aa68818a87629b27632ab029b720
NaviteLogger <kacprzakmarek92@gmail.com>
2024-02-12 11:14:06 +0100

    Fixed the incorrect return statement placement in 'LoginPage.js' file

--- my-app/pages/LoginPage/LoginPage.js
+++ my-app/pages/LoginPage/LoginPage.js
@@ -45,40 +45,40 @@ function LoginPage() {
                 @todo: Add some more meaningful error handling
             **/
         };
-
-        return (
-            <div className="login-container-box">
-                <HomeHeader />
-                <div className="login-container">
-                    <form className="login-form" onSubmit={handleSubmit}>
-                        <h2 className="login-title">Login</h2>
-                        <div className="input-group">
-                            <input
-                                name="username"
-                                type="text"
-                                placeholder="Username"
-                                value={credentials.username}
-                                onChange={handleChange}
-                                required
-                            />
-                        </div>
-                        <div className="input-group">
-                            <input
-                                name="password"
-                                type="password"
-                                placeholder="Password"
-                                value={credentials.password}
-                                onChange={handleChange}
-                                required
-                            />
-                        </div>
-                        <button className="login-button" type="submit">Login</button>
-                    </form>
-                </div>
-                <Footer />
-            </div>
-        );
     }
+
+    return (
+        <div className="login-container-box">
+            <HomeHeader />
+            <div className="login-container">
+                <form className="login-form" onSubmit={handleSubmit}>
+                    <h2 className="login-title">Login</h2>
+                    <div className="input-group">
+                        <input
+                            name="username"
+                            type="text"
+                            placeholder="Username"
+                            value={credentials.username}
+                            onChange={handleChange}
+                            required
+                        />
+                    </div>
+                    <div className="input-group">
+                        <input
+                            name="password"
+                            type="password"
+                            placeholder="Password"
+                            value={credentials.password}
+                            onChange={handleChange}
+                            required
+                        />
+                    </div>
+                    <button className="login-button" type="submit">Login</button>
+                </form>
+            </div>
+            <Footer />
+        </div>
+    );
 }
 
 export default LoginPage;
