90889e6f5e912d4cc59875d9d0a4f844254b857e
NaviteLogger <kacprzakmarek92@gmail.com>
2024-02-12 10:53:19 +0100

    Added database test route. Removed pymysql from dependencies

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,10 +1,23 @@
-from flask import Blueprint, render_template, current_app, request, jsonify, session
-import requests
-
+from flask import Blueprint, current_app
+from sqlalchemy import text
+from app import db
 
 main_bp = Blueprint("main_bp", __name__)
 
 
+# @main_bp.route("/")
+# def index():
+#     return render_template("index.html")
+
+
 @main_bp.route("/")
-def index():
-    return render_template("index.html")
+def test_database_connection():
+    with current_app.app_context():
+        try:
+            sql = text("SELECT VERSION()")
+            result = db.session.execute(sql)
+            version = result.fetchone()[0]
+
+            return "Database connection is working", 200
+        except Exception as e:
+            return f"Database connection is not working: {str(e)}", 500
