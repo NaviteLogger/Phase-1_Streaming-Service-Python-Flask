febdf929c4c706c071d39a06b76b0419da166ed4
NaviteLogger <kacprzakmarek92@gmail.com>
2024-02-17 18:36:26 +0100

    Added testing to app client

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -2,3 +2,19 @@ import pytest
 from flask import url_for
 from app import create_app, db
 from app.users.models import User
+
+
+@pytest.fixture
+def client():
+    app = create_app(TestConfig)
+    app.config["TESTING"] = True
+    client = app.test_client()
+
+    with app.app_context():
+        db.create_all()  # Create test database tables
+        # Optionally, you can preload some test data here
+
+    yield client
+
+    with app.app_context():
+        db.drop_all()  # Clean up the database
