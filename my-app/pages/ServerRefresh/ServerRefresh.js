a3bf5cd19863f5109e58ce1c1d7dd32fd987c176
NaviteLogger <kacprzakmarek92@gmail.com>
2024-02-20 11:32:14 +0100

    Aded export content to ServerRefresh component

---
+++ my-app/pages/ServerRefresh/ServerRefresh.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function ServerRefresh() {
+    return (
+        <div className="server-refresh">
+            <section className="server-refresh">
+                <button>Refresh</button>
+            </section>
+        </div>
+    );
+}
+
+export default ServerRefresh;
