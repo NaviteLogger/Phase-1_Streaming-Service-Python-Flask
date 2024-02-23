e14f81ea4058478a8c6f54b03a31e742752720d7
NaviteLogger <kacprzakmarek92@gmail.com>
2024-02-23 17:56:17 +0100

    Begun working on /bookmark-movie route for app/movies

--- app/movies/routes.py
+++ app/movies/routes.py
@@ -8,16 +8,24 @@ def search_for_movie():
     query = request.args.get("query", "")
 
     if not query:
-        return jsonify({"error": "Missing query parameter"}), 400
+        return jsonify({"status": "error", "message": "Missing query parameter"}), 400
 
     # Search for movies that match the query
     movies = Movie.query.filter(Movie.title.ilike(f"%{query}%")).limit(5).all()
 
     # Check whether any movies were found
     if not movies:
-        return jsonify({"error": "No movies found"}), 404
+        return jsonify({"status": "error", "message": "No movies found"}), 404
 
     # Serialize the movies to JSON
     movies_json = jsonify([movie.serialize() for movie in movies])
 
     return movies_json, 200
+
+
+@movies_bp.route("/bookmark-movie", methods=["POST"])
+def bookmark_movie():
+    data = request.get_json()
+
+    if not data:
+        return jsonify("status": "error", "message": "Missing JSON body"}), 400
